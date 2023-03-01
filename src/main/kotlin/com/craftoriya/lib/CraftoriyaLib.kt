d4075d4befd52ad190568c3cfbd4cb410789d189
nitkanikita21 <waleryvvs1s@gmail.com>
2023-03-01 20:18:12 +0200

    Create entry point

---
+++ src/main/kotlin/com/craftoriya/lib/CraftoriyaLib.kt
@@ -0,0 +1,9 @@
+package com.craftoriya.lib
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CraftoriyaLib: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Craftoriya Library is ready to working")
+    }
+}
