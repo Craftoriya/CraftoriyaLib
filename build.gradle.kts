41c4aa157efe989ccfd10ec53e3e24917679c239
nitkanikita21 <waleryvvs1s@gmail.com>
2023-03-01 20:16:55 +0200

    CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     kotlin("jvm") version "1.7.20"
     id("maven-publish")
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("io.papermc.paperweight.userdev") version "1.4.0"
 }
 
 group = "org.example"
@@ -15,6 +17,7 @@ repositories {
 
 dependencies {
     shadow(kotlin("stdlib"))
+    paperDevBundle("1.19.3-R0.1-SNAPSHOT")
 }
 
 tasks.test {
@@ -35,5 +38,16 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
+    }
+}
+
+bukkit {
+    load = net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder.STARTUP
+    main = "me.kotlin.Main"
+    apiVersion = "1.19"
+    authors = listOf("NitkaNikita")
+    name = "Event7"
+    commands {
+        register("dialog")
     }
 }
